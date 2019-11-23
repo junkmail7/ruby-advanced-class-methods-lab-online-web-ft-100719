4f3da8b6a0270e63e2b1bed6ef7ce92e75fca8fb
William Pannell <lpann5@gmail.com>
2019-11-23 02:37:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,13 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def self.create(name)
+    person = self.new
+    person.name = name
+    @@all << person
+  end
+  
   def save
     self.class.all << self
   end
